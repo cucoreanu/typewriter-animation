fd79a3610218462584de767d84e15331ca9f06df
Nicolae Cucoreanu <nicolae@cucorea.nu>
2023-04-17 17:40:08 +0300

    Add character removal ability

--- src/character-emitter.ts
+++ src/character-emitter.ts
@@ -1,21 +1,37 @@
 export class CharacterEmitter {
-  private interval?: NodeJS.Timer;
-  private currentIndex = 0;
-  private currentValue = "";
+  private interval?: any;
+  private currentIndex: number = 0;
+  private removeCharacterSection = '{r}';
+  private currentValue = '';
 
   constructor(private inputString: string) {}
 
   public subscribe(callback: (value: string) => void, timeout: number): void {
     this.interval = setInterval(() => {
+      // If we are at the end of the string, unsubscribe and return
       if (this.currentIndex === this.inputString.length) {
         this.unsubscribe();
         return;
       }
 
-      this.currentValue += this.inputString[this.currentIndex];
+      const indexOfFirstRemoveCharacter = this.inputString.indexOf(
+        this.removeCharacterSection,
+        this.currentIndex - 1
+      );
+
+      // If we are at the start of the remove character section, remove one character and update current index
+      if (
+        this.inputString[this.currentIndex] === this.removeCharacterSection[0] &&
+        indexOfFirstRemoveCharacter === this.currentIndex
+      ) {
+        this.currentIndex += this.removeCharacterSection.length - 1;
+        this.currentValue = this.currentValue.slice(0, -1);
+      } else {
+        this.currentValue = this.currentValue + this.inputString[this.currentIndex];
+      }
       this.currentIndex++;
 
-      callback(this.currentValue);
+      callback(this.currentValue)
     }, timeout);
   }
 
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import "./style.css"
 import {CharacterEmitter} from "./character-emitter";
 
 const container = document.getElementById("tContainer") as HTMLDivElement;
-const emitter = new CharacterEmitter("Hello, world!");
+const emitter = new CharacterEmitter("Hello, wp{r}orld!");
 emitter.subscribe((value) => (container.innerText = value), 150);
